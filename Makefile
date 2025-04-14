d73ab2f59d1e1c8fe592554564bb7081ac1ecc30
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2025-04-14 17:00:36 +0900

    add:register_endpoint

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
 up:
 	docker compose up -d
+app:
+	docker compose up app -d --build
+exec:
+	docker compose exec -it app sh
 down:
 	docker compose down
 ps:
--- main.go
+++ main.go
@@ -9,9 +9,16 @@ import (
 func main() {
 	r := gin.Default()
 
-	r.GET("/", func(c *gin.Context) {
+	r.GET("/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello, World!",
+			"status": "healthy",
+		})
+	})
+
+	apiGroup := r.Group("/api")
+	apiGroup.POST("/register", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"data": "register endpoint",
 		})
 	})
 
